5852c00d14ae90c6652bd54942fa22829c233512
Miguel Pérez García <miguelpg_95@hotmail.com>
2018-02-13 10:16:11 -0600

    Key-value store perzonalizado

---
+++ src/TuRuta/TuRuta.Orleans.Interfaces/IKeyMapperGrain.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TuRuta.Orleans.Interfaces
+{
+    public interface IKeyMapperGrain
+    {
+        Task<Guid> GetId(string name);
+
+        Task SetName(string name, Guid Id);
+    }
+}
