267aa7d4b23abb199d4129bc3c8b659ceee847e5
Miguel Pérez García <miguelpg_95@hotmail.com>
2018-02-13 10:17:29 -0600

    agregando interfaz al grain

--- src/TuRuta/TuRuta.Orleans.Interfaces/IKeyMapperGrain.cs
+++ src/TuRuta/TuRuta.Orleans.Interfaces/IKeyMapperGrain.cs
@@ -1,11 +1,12 @@
-﻿using System;
+﻿using Orleans;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace TuRuta.Orleans.Interfaces
 {
-    public interface IKeyMapperGrain
+    public interface IKeyMapperGrain : IGrainWithStringKey
     {
         Task<Guid> GetId(string name);
 
