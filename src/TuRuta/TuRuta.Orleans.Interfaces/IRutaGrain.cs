78462d79cf857a6e43b27b29940f804dd2bb04f1
José Francisco Robles Sapién <j.fr.sapi23@gmail.com>
2018-02-13 11:40:30 -0600

    Rutas y paradas

---
+++ src/TuRuta/TuRuta.Orleans.Interfaces/IRutaGrain.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TuRuta.Common.Models;
+using Orleans;
+using System.Threading.Tasks;
+
+namespace TuRuta.Orleans.Interfaces
+{
+    public interface IRutaGrain : IGrainWithGuidKey
+    {
+		Task<List<Parada>> AllParadas();
+    }
+}
