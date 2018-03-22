44e04e0f863a46368db04f791d2c5525b9a2e933
FranciscoRobles <A01225006@itesm.mx>
2018-03-22 17:39:38 -0600

    Stopgrain getRoute

--- src/TuRuta/TuRuta.Orleans.Grains/StopGrain.cs
+++ src/TuRuta/TuRuta.Orleans.Grains/StopGrain.cs
@@ -18,6 +18,7 @@ namespace TuRuta.Orleans.Grains
 		{
 			State.Location = stopVM.Location;
 			State.Name = stopVM.Name;
+			return Task.CompletedTask;
 		}
         public async Task<List<RouteVM>> GetRoutes()
             => (await Task.WhenAll(State.Routes.Select(route => route.GetRouteVM()))).ToList();
@@ -30,6 +31,22 @@ namespace TuRuta.Orleans.Grains
 			var routeGrain = GrainFactory.GetGrain<IRouteGrain>(routeId);
 			State.Routes.Add(routeGrain);
 			return Task.CompletedTask;
+		}
+		public Task<StopVM> GetStopVM()
+		{
+			var vm = new StopVM
+			{
+				Id = this.GetPrimaryKey(),
+				Name = State.Name,
+				Location = State.Location
+				
+			};
+			return Task.FromResult(vm);
+		}
+		public Task<RouteVM> GetRoute()
+		{
+			var vm = State.Routes.Select(route => route.GetRouteVM());
+			return Task.FromResult(vm);
 		}
 	}
 }
