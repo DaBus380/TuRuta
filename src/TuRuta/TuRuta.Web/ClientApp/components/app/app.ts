11763817dccc3c3be6f2cdbef19312e646b52f56
eldelcesar <cesarjr.espinosa@gmail.com>
2018-04-02 04:42:10 -0500

    All basic pages with some interaction

--- src/TuRuta/TuRuta.Web/ClientApp/components/app/app.ts
+++ src/TuRuta/TuRuta.Web/ClientApp/components/app/app.ts
@@ -3,7 +3,8 @@ import { Component } from 'vue-property-decorator';
 
 @Component({
     components: {
-        MenuComponent: require('../navmenu/navmenu.vue.html')
+        MenuComponent: require('../navmenu/navmenu.vue.html'),
+        SearchComponent: Vue.component('search-component', require('../search/search.vue.html'))
     }
 })
 export default class AppComponent extends Vue {
