078559ebd58904443391c250cdc6a7a0ab299dd3
eldelcesar <cesarjr.espinosa@gmail.com>
2018-04-04 18:19:49 -0500

    Route-info Component and Maps with Markers

--- src/TuRuta/TuRuta.Web/ClientApp/components/map/map.ts
+++ src/TuRuta/TuRuta.Web/ClientApp/components/map/map.ts
@@ -1,11 +1,15 @@
 ﻿import Vue from 'vue';
-import { Component } from 'vue-property-decorator';
+import { Component, Prop } from 'vue-property-decorator';
 import { } from "@types/googlemaps";
 
 @Component
 export default class MapComponent extends Vue {
 
+    @Prop()
     map?: google.maps.Map;
+    markers?: google.maps.Marker[];
+    stops?: stopVM[];
+
     mounted() {
         let element = document.getElementById("mapDiv");
         let latLon = new google.maps.LatLng(20.6736, -103.344);
@@ -16,5 +20,31 @@ export default class MapComponent extends Vue {
         };
 
         this.map = new google.maps.Map(element, options);
+
+        let myLatLng = {lat: 20.678819, lng: -103.394160};
+
+        this.markers = this.mapStopsToMarkers();   
     }
+
+    mapStopsToMarkers() {
+        var tempMarkers = new Array<google.maps.Marker>();
+        if (this.stops != undefined) {
+            this.stops.forEach(stop => {
+                var location = { lat: stop.location.latitude, lng: stop.location.longitude }
+                var marker = new google.maps.Marker({
+                    position: location,
+                    map: this.map,
+                    title: stop.name,
+                });
+                tempMarkers.push()
+            });
+        }
+        return tempMarkers
+    }
+
+    /*var marker = new google.maps.Marker({
+            position: myLatLng,
+            map: this.map,
+            title: 'Hello World!'
+          });*/
 }
