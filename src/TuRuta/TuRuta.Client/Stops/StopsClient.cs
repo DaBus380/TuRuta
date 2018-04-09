d28215f4327cde42d50880f65948e916d058e7c9
FranciscoRobles <A01225006@itesm.mx>
2018-04-08 23:10:24 -0500

    Archivo cliente sin completar (no pudo compilarlo)

---
+++ src/TuRuta/TuRuta.Client/Stops/StopsClient.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+using TuRuta.Common.ViewModels;
+
+namespace TuRuta.Client.Stops
+{
+    class StopsClient
+    {
+		public StopsClient(string baseAddress = null)
+		{
+			var address = baseAddress ?? "http://localhost:56340/";
+			HttpClient.BaseAddress = new Uri(address);
+		}
+
+		private HttpClient HttpClient { get; } = new HttpClient();
+	}
+}
