96af2ad6bdecd67ae4794592b203c979a9f6b86d
FranciscoRobles <A01225006@itesm.mx>
2018-04-30 16:47:06 -0500

    BusesClient

--- src/TuRuta/TuRuta.Client/Buses/BusesClient.cs
+++ src/TuRuta/TuRuta.Client/Buses/BusesClient.cs
@@ -1,15 +1,29 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using TuRuta.Common.ViewModels;
 
 namespace TuRuta.Client.Buses
 {
     public class BusesClient
     {
-        public Task<object> Info(Guid busId)
+		public BusesClient(string baseAddress = null)
+		{
+			var address = baseAddress ?? "http://localhost:56340/";
+			HttpClient.BaseAddress = new Uri(address);
+		}
+		private HttpClient HttpClient { get; } = new HttpClient();
+		public async Task<BusInfoVM> Info(Guid busId)
         {
-            throw new NotImplementedException();
+			var response = await HttpClient.GetAsync($"/api/Buses/{busId}");
+			if(response.IsSuccessStatusCode)
+			{
+				return JsonConvert.DeserializeObject<BusInfoVM>(await response.Content.ReadAsStringAsync());
+			}
+			return default(BusInfoVM);
         }
     }
 }
